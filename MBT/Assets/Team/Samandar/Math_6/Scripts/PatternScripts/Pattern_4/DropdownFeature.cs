441abddf3269f8cad387c82f1bb2cfcb60dd5c11
RustamMuhammadFathulloh <98743553+RustamMuhammadFathulloh@users.noreply.github.com>
2022-07-30 18:11:20 +0500

    Pattern 4  DropDown

--- MBT/Assets/Team/Samandar/Math_6/Scripts/PatternScripts/Pattern_4/DropdownFeature.cs
+++ MBT/Assets/Team/Samandar/Math_6/Scripts/PatternScripts/Pattern_4/DropdownFeature.cs
@@ -21,13 +21,33 @@ public class DropdownFeature : MonoBehaviour
     }
     public void TurnOn()
     {
-        //Label.GetComponent<TMP_Text>().enabled = true;
+        Label.GetComponent<TMP_Text>().enabled = true;
         Label.GetComponent<TMP_Text>().color = new Color(1, 1, 1, 1);
         //Arrow.GetComponent<TMP_Text>().color = new Color(1, 1, 1, 1);
-        Debug.Log(Label.GetComponent<TMP_Text>().color);
+        //Debug.Log(Label.GetComponent<TMP_Text>().color);
+        Debug.Log("DropDown");
     }
     public void ColorUpdate()
     {
         
     }
+
+
+    public void CheckIsWorking()
+    {
+        Debug.Log("Item");
+        //Label.GetComponent<TMP_Text>().enabled = true;
+    }
+
+    public void CheckOnScrollBar()
+    {
+        Debug.Log("Scrollbar");
+    }
+
+    public void CheckTemplate()
+    {
+        Debug.Log("Tamplate");
+    }
+
+
 }
