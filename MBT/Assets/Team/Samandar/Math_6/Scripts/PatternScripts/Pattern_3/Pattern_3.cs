726789c4efb40febd21c545b3a2cda91cde51acd
Samandar2001 <samandaribrohimov2001@gmail.com>
2022-07-30 17:37:47 +0500

    Pattern 3
    Update

--- MBT/Assets/Team/Samandar/Math_6/Scripts/PatternScripts/Pattern_3/Pattern_3.cs
+++ MBT/Assets/Team/Samandar/Math_6/Scripts/PatternScripts/Pattern_3/Pattern_3.cs
@@ -5,7 +5,8 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using TexDrawLib;
-
+using System.Linq;
+using UnityEngine.UI;
 
 public class Pattern_3 : TestManager
 {
@@ -13,6 +14,8 @@ public class Pattern_3 : TestManager
     public string sampleQuestion;
     public List<TEXDraw> NumbersText;
     public List<GameObject> Numbers;
+    public List<GameObject> NumbersParent;
+    public GameObject NumberPrefabs;
     public GameObject NumberAreaPrefabs;
     public List<GameObject> NumberArea;
     private GameObject MainParent;
@@ -27,22 +30,22 @@ public class Pattern_3 : TestManager
         var jsonObj = JObject.Parse(jsonText.text);
                
         var likeName = jsonObj["chapters"][1]["questions"][27]["question"]["title"].Value<string>();
-        objaa = jsonObj["chapters"][1]["questions"][27]["question"].ToObject<Data_3>();
+        objaa = jsonObj["chapters"][0]["questions"][21]["question"].ToObject<Data_3>();
         List<string> problem1 = objaa.problem;
-        
-        for (int i = 0; i < NumbersText.Count; i++)
-        {
-            NumbersText[i].transform.GetComponent<TEXDraw>().text = problem1[i].ToString();
-        }
-        ////objaa = new Pattern3Data();
-        //objPrefab();
-        //List<string> solution1 = objaa.solution[0];
 
-        //for (int i = 0; i < solution1.Count - 1; i++)
-        //{
-        //    GameObject obj = Instantiate(NumberAreaPrefabs, NumberArea[i].transform);
-        //}
+        
+
         List<string> solution1 = objaa.solution[0];
+        //string _problem = objaa.problem[0];
+
+        for (int i = 0; i < problem1.Count; i++)
+        {
+            NumbersParent[i].transform.parent.gameObject.transform.GetComponent<HorizontalLayoutGroup>().enabled = false;
+            NumbersParent[i].SetActive(true);
+            GameObject Obj1 = Instantiate(NumberPrefabs, NumbersParent[i].transform);
+            Numbers.Add(Obj1);
+        }
+
 
         for (int i = 0; i < solution1.Count - 1; i++)
         {
@@ -59,8 +62,23 @@ public class Pattern_3 : TestManager
         }
         for (int i = 0; i < Numbers.Count; i++)
         {
+            Numbers[i].GetComponent<DegnDropPattern_3>().Pattern3 = this;
             Numbers[i].GetComponent<DegnDropPattern_3>().Positions = NumberInstantiate;
         }
+
+        for (int i = 0; i < problem1.Count; i++)
+        {
+            NumbersParent[i].transform.GetChild(0).transform.GetChild(0).GetComponent<TEXDraw>().text = problem1[i].ToString();
+        }
+        ////objaa = new Pattern3Data();
+        //objPrefab();
+        //List<string> solution1 = objaa.solution[0];
+
+        //for (int i = 0; i < solution1.Count - 1; i++)
+        //{
+        //    GameObject obj = Instantiate(NumberAreaPrefabs, NumberArea[i].transform);
+        //}
+
     }
 
     private void OnEnable()
