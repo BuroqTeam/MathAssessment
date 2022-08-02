2aa3e0f3cd19713e9414d743956816450ddcff42
Samandar2001 <samandaribrohimov2001@gmail.com>
2022-08-02 17:18:31 +0500

    Pattern 7
    update

---
+++ MBT/Assets/Team/Samandar/Math_6/Scripts/PatternScripts/Pattern_7/PenTool.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PenTool : MonoBehaviour
+{
+    public PointsPattern_7 pointsPattern7;
+    public GameObject linePrefabs;
+    public GameObject Point;
+    public RectTransform lineParent;
+    public RectTransform dotParent;
+    private LineControllerPattern_7 currentLine;
+    public Pattern_7 managerPattern7;
+    public PenCanvas penCanvas;
+
+    void Start()
+    {
+        penCanvas.OnPenCanvasLeftClickEvent += AddDot;
+    }
+
+    public void AddDot()
+    {
+        if (currentLine == null)
+        {
+            currentLine = Instantiate(linePrefabs, Vector3.zero, Quaternion.identity, lineParent).GetComponent<LineControllerPattern_7>();
+            managerPattern7.False();
+        }
+        GameObject dot = Instantiate(Point, GetMousePosition(), Quaternion.identity, dotParent);
+        dot.transform.localPosition = new Vector3(dot.transform.localPosition.x, dot.transform.localPosition.y, dot.transform.localPosition.z);  
+        pointsPattern7.Check();
+        currentLine.AddPoint(dot.transform);
+    }
+
+
+
+    public Vector3 GetMousePosition()
+    {
+        Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldMousePosition.z = 0;
+        pointsPattern7.Check();
+        return worldMousePosition;
+    }
+
+    // Update is called once per frame
+
+}
+
