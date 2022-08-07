5ea7b7fff66405d218ce4051979305b85de91d22
Jahongir1909 <jahongirmardonov2003@gmail.com>
2022-08-07 17:43:59 +0500

    P6
    Done

--- MBT/Assets/Team/Jahongir/Scripts/Pattern_6.cs
+++ MBT/Assets/Team/Jahongir/Scripts/Pattern_6.cs
@@ -50,6 +50,23 @@ public class Pattern_6 : MonoBehaviour
             Debug.Log("Correct");
         }
     }
+    void Check()
+    {
+        Result();
+        List<bool> myList = new List<bool>();
+        ES3.Save("ResultList", myList);
+        List<bool> currentList = new List<bool>();
+        currentList = ES3.Load<List<bool>>("ResultList");
+        if (transform.GetChild(1).GetChild(0).GetComponent<TEXDraw>().text == Pattern_6Obj.solution[0])
+        {
+            currentList[GetComponent<Pattern>().QuestionNumber] = true;
+        }
+        else
+        {
+            currentList[GetComponent<Pattern>().QuestionNumber] = false;
+        }
+        ES3.Save("myList", currentList);
+    }
 }
 
 [SerializeField]
