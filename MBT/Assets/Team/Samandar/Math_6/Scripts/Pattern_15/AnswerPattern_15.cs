bcb17eecafa23d29ac9d4b439ebcb9a52542caa9
Samandar2001 <samandaribrohimov2001@gmail.com>
2022-08-07 12:21:19 +0500

    Pattern 15

---
+++ MBT/Assets/Team/Samandar/Math_6/Scripts/Pattern_15/AnswerPattern_15.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswerPattern_15 : MonoBehaviour
+{
+    public Pattern_15 Pattern15;
+    public bool _PattenBool;
+    Button button;
+    public ColorCollectionSO colorCollection;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(TaskOnClick);
+    }
+
+    public void TaskOnClick()
+    {
+        foreach (Button btn in Pattern15.buttonGroup)
+        {
+            if (Pattern15.buttonGroup.Count == 3)
+            {
+                if (button.Equals(btn))
+                {
+                    btn.GetComponent<Image>().color = colorCollection.Blue;
+                    btn.gameObject.transform.GetChild(0).GetComponent<TEXDraw>().color = colorCollection.White;
+                }
+
+                else
+                {
+                    btn.GetComponent<Image>().color = colorCollection.White;
+                    btn.transform.GetChild(0).GetComponent<TEXDraw>().color = colorCollection.Green;
+                }
+            }
+            else if (Pattern15.buttonGroup.Count == 2)
+            {
+                if (button.Equals(btn))
+                {
+                    btn.GetComponent<Image>().color = colorCollection.Blue;
+                    btn.gameObject.transform.GetChild(0).GetComponent<TEXDraw>().color = colorCollection.White;
+                }
+                else
+                {
+                    btn.GetComponent<Image>().color = colorCollection.White;
+                    btn.transform.GetChild(0).GetComponent<TEXDraw>().color = colorCollection.Green;
+                    Pattern15.buttonGroup[1].transform.GetChild(0).GetComponent<TEXDraw>().color = colorCollection.Red;
+                }
+            }
+            else
+            {
+                if (button.Equals(btn))
+                {
+                    btn.GetComponent<Image>().color = colorCollection.Blue;
+                    btn.gameObject.transform.GetChild(0).GetComponent<TEXDraw>().color = colorCollection.White;
+                }
+
+                else
+                {
+                    btn.GetComponent<Image>().color = colorCollection.White;
+                    btn.transform.GetChild(0).GetComponent<TEXDraw>().color = colorCollection.Green;
+                }
+            }
+        }
+    }
+
+
+
+    public void Check()
+    {
+        if (_PattenBool)
+        {
+            Debug.Log("Corrent");
+        }
+        else
+        {
+            Debug.Log("Wrong");
+        }
+
+    }
+
+
+
+}
