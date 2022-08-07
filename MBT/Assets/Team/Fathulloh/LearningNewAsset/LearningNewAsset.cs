6445d26663fddac08c3de88350d77df40557c234
RustamMuhammadFathulloh <98743553+RustamMuhammadFathulloh@users.noreply.github.com>
2022-08-07 18:52:12 +0500

    ES3 boshlandi.

---
+++ MBT/Assets/Team/Fathulloh/LearningNewAsset/LearningNewAsset.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningNewAsset : MonoBehaviour
+{
+    public List<string> ProgramLanguages = new List<string>() { "Php", "C#", "C++" };
+
+
+
+    void Start()
+    {
+        SaveSomething();
+    }
+
+
+    void SaveSomething()
+    {
+
+        int nulValue = 0;
+        //ES3.Save("myIntF", 8000);
+        int myInt = ES3.Load("myIntF", nulValue);
+        Debug.Log("myIntF = " + myInt);
+
+        ES3.Save("myIntF", 2022);
+
+        Debug.Log("Yangi " + ES3.Load("myIntF"));
+
+
+        //if (ES3.KeyExists("ProgrammingLanguages", ))
+        //{
+        //    Debug.Log("Data bor.");
+        //}
+        //else
+        //{
+        //    Debug.Log("Data yo'q.");
+        //}
+
+
+    }
+
+
+
+
+
+}
