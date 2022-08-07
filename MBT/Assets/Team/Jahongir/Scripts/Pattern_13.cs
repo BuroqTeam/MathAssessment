a98f62d53f1d1951726b6aa6f0d3d065df340303
Jahongir1909 <jahongirmardonov2003@gmail.com>
2022-08-07 18:43:53 +0500

    Prefabs
    Update

--- MBT/Assets/Team/Jahongir/Scripts/Pattern_13.cs
+++ MBT/Assets/Team/Jahongir/Scripts/Pattern_13.cs
@@ -1,18 +1,51 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using MBT.Extension;
+using System;
 
 public class Pattern_13 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject PuzzleQuestion;
+    public GameObject PuzzleAnswer;
+    public TextAsset _currentJsonText;
+    private bool _isTrue = true;
+    Data_13 Pattern_13Obj = new Data_13();
+
+    private void OnEnable()
     {
-        
+        if (_isTrue)
+        {
+            _isTrue = false;
+            _currentJsonText = GetComponent<Pattern>().Json;
+            if (_currentJsonText != null)
+            {
+                Debug.Log(_currentJsonText.text);
+            }
+            else
+            {
+                Debug.Log("Not Found Data");
+            }
+            ReadFromJson();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ReadFromJson()
     {
-        
+        var jsonObj = JObject.Parse(_currentJsonText.text);
+        JObject jo = Mbt.LoadJsonPath(jsonObj, "Pattern_13");
+        Pattern_13Obj = jo.ToObject<Data_13>();
     }
+
+}
+[SerializeField]
+public class Data_13
+{
+    public string title;
+    public List<string> options;
 }
