85d4f35c0fcd591223370bff5b4ea851aac888a5
Samandar2001 <samandaribrohimov2001@gmail.com>
2022-09-03 17:59:00 +0500

    Pattern_7

--- MBT/Assets/Team/Samandar/_Math6/Pattern 7/Scripts/PointsPattern7.cs
+++ MBT/Assets/Team/Samandar/_Math6/Pattern 7/Scripts/PointsPattern7.cs
@@ -1,33 +1,21 @@
 using DG.Tweening;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class PointsPattern7 : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
     public Pattern_7 Pattern_7;
-    private Transform _initial;
-    private Transform _initialPosition;
+  
     private GameObject Pos_1;
     private GameObject Pos_2;
+
+    public Vector3 LastPosition;
+
     public void OnBeginDrag(PointerEventData eventData)
     {
-        _initial = GetComponent<Transform>();
-        Debug.Log(_initial.position.x);
-        Debug.Log(_initial.position.y);
+      
         Pos_1 = Pattern_7.CellObj[0];
         Pos_2 = Pattern_7.CellObj[99];
-    }
-    private void Awake()
-    {
-        _initialPosition = GetComponent<Transform>();
-    }
-    public void OnEndDrag(PointerEventData eventData)
-    {
-       
-        Check();
-        CheckPosition();
     }
 
     private void Start()
@@ -35,28 +23,40 @@ public class PointsPattern7 : MonoBehaviour, IDragHandler, IBeginDragHandler, IE
         Check();
     }
 
-    void CheckPosition()
+
+
+    public void OnEndDrag(PointerEventData eventData)
     {
+       
+        Check();
+        BackToLastPosition();
+    }
+
+  
+
+    void BackToLastPosition()
+    {
+        
         if (gameObject.transform.position.x < Pos_1.transform.GetComponent<CellPattern7>().points[3].x)
         {
             Debug.Log("1");
-            _initialPosition.position = new Vector3(_initial.position.x, _initial.position.y, 0);
-            Debug.Log(_initialPosition.position);
+            //_initialPosition.position = new Vector3(_initial.position.x, _initial.position.y, 0);
+            transform.DOMove(LastPosition, 0);
         }
-        else if(gameObject.transform.position.x > Pos_2.transform.GetComponent<CellPattern7>().points[0].x)
+        else if (gameObject.transform.position.x > Pos_2.transform.GetComponent<CellPattern7>().points[0].x)
         {
             Debug.Log("2");
-            _initialPosition.position = new Vector2(_initial.position.x, _initial.position.y);
+            transform.DOMove(LastPosition, 0);
         }
         else if (gameObject.transform.position.y < Pos_1.transform.GetComponent<CellPattern7>().points[3].y)
         {
             Debug.Log("3");
-            _initialPosition.position = new Vector2(_initial.position.x, _initial.position.y);
+            transform.DOMove(LastPosition, 0);
         }
         else if (gameObject.transform.position.y > Pos_2.transform.GetComponent<CellPattern7>().points[0].y)
         {
             Debug.Log("4");
-            _initialPosition.position = new Vector2(_initial.position.x, _initial.position.y);
+            transform.DOMove(LastPosition, 0);
         }
     }
 
@@ -69,10 +69,12 @@ public class PointsPattern7 : MonoBehaviour, IDragHandler, IBeginDragHandler, IE
                 if (Vector3.Distance(transform.position, aPoint) <= 0.4f)
                 {
                     transform.position = aPoint;
+                    LastPosition = aPoint;
                     break;
                 }
             }
-        }       
+        }
+        
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -82,10 +84,6 @@ public class PointsPattern7 : MonoBehaviour, IDragHandler, IBeginDragHandler, IE
     }
    
 
-    void Update()
-    {
-
-    }
 
     
 }
