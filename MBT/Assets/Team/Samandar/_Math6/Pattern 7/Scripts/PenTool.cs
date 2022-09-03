89574c8dfcda94aa212312879290c1208752ce02
Samandar2001 <samandaribrohimov2001@gmail.com>
2022-09-03 11:33:15 +0500

    Pattern_7

--- MBT/Assets/Team/Samandar/_Math6/Pattern 7/Scripts/PenTool.cs
+++ MBT/Assets/Team/Samandar/_Math6/Pattern 7/Scripts/PenTool.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+;
 
 public class PenTool : MonoBehaviour
 {
     public PointsPattern7 pointsPattern7;
     public GameObject linePrefabs;
     public GameObject Point;
-    public Transform lineParent;
-    public Transform dotParent; 
+    public GameObject lineParent;
+    public GameObject dotParent; 
     private LineControllerPattern7 currentLine;
     public Pattern_7 Pattern7;
     public PenCanvas penCanvas;
@@ -26,12 +28,16 @@ public class PenTool : MonoBehaviour
         
          if (currentLine == null)
          {
-            currentLine = Instantiate(linePrefabs, Vector3.zero, Quaternion.identity, lineParent).GetComponent<LineControllerPattern7>();
+            currentLine = Instantiate(linePrefabs, Vector3.zero, Quaternion.identity, lineParent.transform).GetComponent<LineControllerPattern7>();
             Pattern7.False();
          }
-         GameObject dot = Instantiate(Point, GetMousePosition(), Quaternion.identity, dotParent);
+         GameObject dot = Instantiate(Point, GetMousePosition(), Quaternion.identity, dotParent.transform);
          pointsPattern7.Check();
          currentLine.AddPoint(dot.transform);
+         if (Pattern7.CanvasOut[0].transform.childCount == Pattern7.Data7.options.Count)
+         {
+            Pattern7.Buttons[0].GetComponent<Button>().interactable = false;
+         }
     }
 
   
